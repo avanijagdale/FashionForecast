f2fd7baa86e440a945e3f51c22605ad853c35922
Alana Gorukanti <alanagorukanti@gmail.com>
2023-10-30 11:03:02 -0400

    website UI code

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const key = "c237dfe95bb48e2913f8dee9f3ed8465"
 const url = "https://api.openweathermap.org/data/2.5/weather?q=";
 const keyParam = "&appid=" + key;
 const units = "&units=imperial";
+const temperature = 0; 
 
 function getWeather() {
   const input = document.querySelector("input").value;
@@ -19,15 +20,27 @@ function getWeather() {
     }
   }).then((jsonResponse) => {
       formatJson(jsonResponse);
+      generateOutfit(jsonResponse);
   });
 }
 
+function generateOutfit(jsonResponse){
+  //var t = getTemp(jsonResponse);
+  //var d = getDesc(jsonResponse);
+  alert(getTemp(jsonResponse));
+}
+
+function getTemp(jsonResponse){
+    return jsonResponse.main["temp"];
+}
+
+function getDesc(jsonResponse){
+  return jsonResponse.weather[0].description;
+}
+
 function formatJson(jsonResponse) {
-  description.innerHTML = "Description: "  + jsonResponse.weather[0].description 
-
-  temp.innerHTML = "Temperature: "  + jsonResponse.main["temp"]
-
-  pressure.innerHTML = "Pressure: "  + jsonResponse.main["pressure"] 
-
-  humidity.innerHTML = "Humidity: "  + jsonResponse.main["humidity"]
+  description.innerHTML = "Description: "  + jsonResponse.weather[0].description;
+  temp.innerHTML = "Temperature: "  + jsonResponse.main["temp"];
+  pressure.innerHTML = "Pressure: "  + jsonResponse.main["pressure"];
+  humidity.innerHTML = "Humidity: "  + jsonResponse.main["humidity"];
 }
